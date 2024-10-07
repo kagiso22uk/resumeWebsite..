f1b2c84e3a3394e7bb5f8f8d1e44d45c4a823856
Mastermindddddd <malusicephas17@gmail.com>
2024-10-08 00:37:01 +0200

    ...

--- script.js
+++ script.js
@@ -4,3 +4,12 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+  event.preventDefault(); // Prevent form submission
+
+  // Display the success message
+  document.getElementById('successMessage').style.display = 'block';
+
+  // Optionally, clear the form inputs
+  document.getElementById('contactForm').reset();
+});
